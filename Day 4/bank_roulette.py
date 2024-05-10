ede0b075a445223a4ad96ca4976185d20ace3284
Captain Chris <50490928+Atredies@users.noreply.github.com>
2024-05-10 18:00:50 +0300

    Day 4 complete

---
+++ Day 4/bank_roulette.py
@@ -0,0 +1,11 @@
+
+# names_string contains the input values provided. 
+# The code above converts the input into an array seperating
+# each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+
+import random
+
+i = random.randint(0, len(names) - 1)
+
+print(f"{names[i]} is going to buy the meal today!")
---
+++ Day 4/heads_or_tails.py
@@ -0,0 +1,8 @@
+import random
+
+generator = random.randrange(0,2)
+
+if generator == 1:
+    print("Heads")
+else:
+    print("Tails")
